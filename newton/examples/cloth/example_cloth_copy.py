93b375c693849cbd72f3444809bba6daf4a56e67
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2025-09-04 17:12:06 -0400

    Correctly add box

--- newton/examples/cloth/example_cloth_copy.py
+++ newton/examples/cloth/example_cloth_copy.py
@@ -21,6 +21,7 @@
 #
 ###########################################################################
 
+from newton._src.geometry.types import GeoType
 import warp as wp
 
 import newton
@@ -28,6 +29,7 @@ import newton.examples
 
 
 class Example:
+
     def draw_ground_rectangle(self):
         """Draws a rectangle outline on the ground matching the cloth's initial position, orientation, and size."""
         # Use the same parameters as the cloth
@@ -44,10 +46,11 @@ class Example:
             wp.array([wp.transform(pos, rot)], dtype=wp.transform),
             wp.array([color], dtype=wp.vec3),
         )
+
     def __init__(
         self,
         viewer,
-        solver_type: str = "vbd",
+        solver_type: str = "xpbd",
         height=64,
         width=32,
     ):
@@ -88,8 +91,8 @@ class Example:
 
         # common cloth properties
         common_params = {
-            "pos": wp.vec3(0.0, 0.0, 4.0),
-            "rot": wp.quat_from_axis_angle(wp.vec3(1.0, 0.0, 0.0), wp.pi*0), #wp.pi/2),
+            "pos": wp.vec3(0,0.1*self.sim_height/2,2.0),#middle drop don't mess with the position (0,0,4.0) to generate in top
+            "rot": wp.quat_from_axis_angle(wp.vec3(1.0, 0.0, 0.1), wp.pi/2), #wp.pi*0), for flat
             "vel": wp.vec3(0.0, 0.0, 0.0),
             "dim_x": self.sim_width,
             "dim_y": self.sim_height,
@@ -101,6 +104,14 @@ class Example:
             "edge_kd": 0.0,
             "particle_radius": 0.05,
         }
+
+        # Add a box above the cloth using the simple approach from example_basic_shapes.py
+        drop_z = 2.0
+        cfg = builder.default_shape_cfg.copy()
+        cfg.density = 3.0
+        body_box = builder.add_body(xform=wp.transform(p=wp.vec3(1.0, 2.0, drop_z), q=wp.quat_identity()))
+        builder.add_shape_box(body_box, hx=0.2, hy=0.2, hz=0.2)
+        
 
         solver_params = {}
         if self.solver_type == "euler":
@@ -166,7 +177,7 @@ class Example:
         self.contacts = self.model.collide(self.state_0)
 
         self.viewer.set_model(self.model)
-
+        
         self.capture()
 
     def capture(self):
